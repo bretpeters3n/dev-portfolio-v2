04012ed079456729c3c78ce4296c7e44190e5ab0
Bret Petersen <bretpetersen82@gmail.com>
2024-04-09 12:51:55 -0500

    Remove autoplay from BM video. Hamburger now fixed to top of screen

--- components/PostVideo.tsx
+++ components/PostVideo.tsx
@@ -1,18 +1,15 @@
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  AwaitedReactNode,
-} from 'react'
+import { React } from 'react'
 
 const PostVideo = (props) => {
   return (
-    <iframe
-      title="project video"
-      className="w-full aspect-video ..."
-      src={props.video}
-    ></iframe>
+    // <iframe
+    //   title="project video"
+    //   className="w-full aspect-video ..."
+    //   src={props.video}
+    // ></iframe>
+    <video controls>
+      <source src={props.video} type="video/mp4" />
+    </video>
   )
 }
 
